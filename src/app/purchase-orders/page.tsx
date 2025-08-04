ad4e04fffd4899b185b882c1145a60b66a927457
M Rizky <muhamrizky00@gmail.com>
2025-08-04 12:29:52 +0000

    sisikanan masih sangat luas

--- src/app/purchase-orders/page.tsx
+++ src/app/purchase-orders/page.tsx
@@ -7,7 +7,7 @@ export default async function PurchaseOrdersPage() {
   const purchaseOrders = await getPurchaseOrders();
 
   return (
-    <div className="h-full flex-1 flex-col space-y-8 p-4 md:p-8 flex">
+    <div className="w-full max-w-7xl mx-auto h-full flex-1 flex-col space-y-8 p-4 md:p-8 flex">
        <div className="flex items-center justify-between space-y-2">
         <div>
           <h2 className="text-2xl font-bold tracking-tight">Purchase Orders</h2>
