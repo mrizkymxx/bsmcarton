7f3f033bf78cacedfa504eb3ad69c1bf13639f62
M Rizky <muhamrizky00@gmail.com>
2025-08-07 07:57:05 +0000

    https://unsplash.com/id/foto/jalan-setapak-kayu-berkelok-kelok-melalui-h

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -40,7 +40,7 @@ export default function LoginPage() {
   return (
     <div 
       className="flex items-center justify-center min-h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('https://images.unsplash.com/photo-1579546929518-9e396f3cc809?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"}}
+      style={{ backgroundImage: "url('https://images.unsplash.com/photo-1548598132-75276f528dba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"}}
     >
       <Card className="w-full max-w-sm mx-4 bg-black/30 backdrop-blur-lg border-white/20 text-white animate-fade-in-up">
         <CardHeader className="text-center">
